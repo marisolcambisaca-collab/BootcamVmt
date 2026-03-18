17f285a215538e76ad4ae8b9dd2860ca61779f33
marisolcambisaca-collab <marisolcambisaca@gmail.com>
2026-03-18 16:50:40 -0500

    error de subida de archivo

---
+++ Taller/Taller/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1 calculadora()
+	{
+
+		double Sumar(double a, double b)		=> a + b ;
+		double Restar(double a, double b)		=> a - b ;
+		double Multiplicar(double a, double b)	=> a *b  ;
+		double Dividir (double a, double b)     => a / b ; 
+	
+
+		return new Class1();
+
+	}
+
+	public class 
+}
---
+++ Taller/Taller/Interface1.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace CsharpIntroduction.Startup.Interfaces
+{
+    internal interface Interface1
+    {
+        double Sumar(double a, double b) => a + b;
+        double Restar(double a, double b) => a - b;
+        double Multiplicar(double a, double b) => a * b;
+        double Dividir(double a, double b) => a / b;
+
+    }
+}
