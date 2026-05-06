6044b2a691d39bf0d14dc9bab4f9abf32b0476a9
Marisol <nurymarisol123@gmail.com>
2026-05-06 00:56:27 -0500

    limpio archivos de la raiz

--- src/app/interfaces/department.ts
+++
@@ -1,8 +0,0 @@
-export interface Department {
- 
-    id:         string;
-    name:       string;
-    description:string;
-    managerName:string;
-    createdAt:  string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://69f83bd3dd0c226688ee46dc.mockapi.io/api/v1' 
-};
